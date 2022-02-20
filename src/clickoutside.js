425faf81ec27d2e6e9067c640ec6fba1aa67ad74
Alireza Kasir <alirezakasir@yahoo.com>
2022-02-20 20:51:44 +0330

    add to script loading window event

--- src/clickoutside.js
+++ src/clickoutside.js
@@ -1,22 +1,24 @@
-// get elements
-let allElements = document.querySelectorAll("*");
-allElements = Array.from(allElements);
-// list the elements have clickoutside event
-let elementsHaveClickoutsideEvent = [];
-allElements.forEach((element) => {
-  let clickEvent = element.getEventListeners().clickoutside;
-  if (clickEvent) {
-    elementsHaveClickoutsideEvent.push(element);
-  }
-});
-// create the clickoutside event
-const clickoutsideEvent = new CustomEvent("clickoutside");
-// target the event
-document.addEventListener("click", (e) => {
-  elementsHaveClickoutsideEvent.forEach((element) => {
-    let isClickInsideElement = element.contains(e.target);
-    if (!isClickInsideElement) {
-      element.dispatchEvent(clickoutsideEvent);
+window.addEventListener("load", () => {
+  // get elements
+  let allElements = document.querySelectorAll("*");
+  allElements = Array.from(allElements);
+  // list the elements have clickoutside event
+  let elementsHaveClickoutsideEvent = [];
+  allElements.forEach((element) => {
+    let clickEvent = element.getEventListeners().clickoutside;
+    if (clickEvent) {
+      elementsHaveClickoutsideEvent.push(element);
     }
+  });
+  // create the clickoutside event
+  const clickoutsideEvent = new CustomEvent("clickoutside");
+  // target the event
+  document.addEventListener("click", (e) => {
+    elementsHaveClickoutsideEvent.forEach((element) => {
+      let isClickInsideElement = element.contains(e.target);
+      if (!isClickInsideElement) {
+        element.dispatchEvent(clickoutsideEvent);
+      }
+    });
   });
 });
