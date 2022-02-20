6f458f570efa4c9433b7b6de73d3d83ecde1eed6
Alireza Kasir <alirezakasir@yahoo.com>
2022-02-20 22:41:19 +0330

    delete  the dependency geteventlistener and edit code with new mode

--- src/clickoutside.js
+++ src/clickoutside.js
@@ -1,24 +1,28 @@
-window.addEventListener("load", () => {
-  // get elements
-  let allElements = document.querySelectorAll("*");
-  allElements = Array.from(allElements);
-  // list the elements have clickoutside event
-  let elementsHaveClickoutsideEvent = [];
-  allElements.forEach((element) => {
-    let clickEvent = element.getEventListeners().clickoutside;
-    if (clickEvent) {
-      elementsHaveClickoutsideEvent.push(element);
+// list the elements have clickoutside event
+let elementsHaveClickoutsideEvent = [];
+// save the original methods before overwriting them
+Element.prototype._addEventListener = Element.prototype.addEventListener;
+
+Element.prototype.addEventListener = function (
+  type,
+  listener,
+  useCapture = false
+) {
+  // declare listener
+  this._addEventListener(type, listener, useCapture);
+  if (type == "clickoutside" && !elementsHaveClickoutsideEvent.includes(this)) {
+    elementsHaveClickoutsideEvent.push(this);
+  }
+};
+
+// create the clickoutside event
+const clickoutsideEvent = new CustomEvent("clickoutside");
+// target the event
+document.addEventListener("click", (e) => {
+  elementsHaveClickoutsideEvent.forEach((element) => {
+    let isClickInsideElement = element.contains(e.target);
+    if (!isClickInsideElement) {
+      element.dispatchEvent(clickoutsideEvent);
     }
-  });
-  // create the clickoutside event
-  const clickoutsideEvent = new CustomEvent("clickoutside");
-  // target the event
-  document.addEventListener("click", (e) => {
-    elementsHaveClickoutsideEvent.forEach((element) => {
-      let isClickInsideElement = element.contains(e.target);
-      if (!isClickInsideElement) {
-        element.dispatchEvent(clickoutsideEvent);
-      }
-    });
   });
 });
