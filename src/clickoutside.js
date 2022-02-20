71ed3d93cda23a1c552c115d204a98b6ab4019e5
Alireza Kasir <alirezakasir@yahoo.com>
2022-02-20 20:38:05 +0330

    delete functions from array elements have clickoutside event

--- src/clickoutside.js
+++ src/clickoutside.js
@@ -6,23 +6,17 @@ let elementsHaveClickoutsideEvent = [];
 allElements.forEach((element) => {
   let clickEvent = element.getEventListeners().clickoutside;
   if (clickEvent) {
-    let doingFunctions = [];
-    element.getEventListeners().clickoutside?.forEach((e) => {
-      doingFunctions.push(e.listener);
-    });
-    const newItem = { element: element, functions: doingFunctions };
-    elementsHaveClickoutsideEvent.push(newItem);
+    elementsHaveClickoutsideEvent.push(element);
   }
 });
 // create the clickoutside event
 const clickoutsideEvent = new CustomEvent("clickoutside");
 // target the event
 document.addEventListener("click", (e) => {
-  elementsHaveClickoutsideEvent.forEach((item) => {
-    let element = item.element;
-    let isClickInsideElement = element.contains(event.target);
+  elementsHaveClickoutsideEvent.forEach((element) => {
+    let isClickInsideElement = element.contains(e.target);
     if (!isClickInsideElement) {
-      item.element.dispatchEvent(clickoutsideEvent);
+      element.dispatchEvent(clickoutsideEvent);
     }
   });
 });
